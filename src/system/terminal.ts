a5c590e9b92b6d21511103ad2ef404c15557b5a4
Bernhard Walter <bernhard.walter@databricks.com>
2020-09-02 15:27:19 +0200

    output shown synchronously

--- src/system/terminal.ts
+++ src/system/terminal.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
-import { exec } from 'child_process';
-import { promises } from 'dns';
+import { exec, spawn } from 'child_process';
+import * as output from '../databricks/Output';
 
 export class TerminalExecute {
     writeEmitter = new vscode.EventEmitter<string>();
@@ -18,15 +18,20 @@ export class TerminalExecute {
     async execute(command: string): Promise<void> {
         return new Promise((resolve, reject) => {
             this.terminal.show();
-            exec(command, (error, stdout, stderr) => {
-                if (error) {
-                    this.print(error.message);
-                }
-                if (stdout) {
-                    this.print(stdout);
-                }
-                if (stderr) {
-                    this.print(stderr);
+            const child = spawn(command, {
+                stdio: 'pipe', shell: true
+            });
+            child.stdout?.on('data', data => {
+                this.print(data.toString());
+            });
+            child.stderr?.on('data', data => {
+                this.print(data.toString());
+            });
+            child.on('exit', (code, signal) => {
+                if (code === 0) {
+                    output.info(`Successfully executed '${command}`);
+                } else {
+                    output.error(`Failed to execute '${command}(${code})`);
                 }
                 resolve();
             });
@@ -35,7 +40,6 @@ export class TerminalExecute {
 
     print(msg: string) {
         for (let line of msg.split(/\r?\n/)) {
-            this.writeEmitter.fire('\x1b[D');
             this.writeEmitter.fire(line + "\r\n");
         }
     }
